e1e1005853bb52264b82d36c09aa04b67528e101
Bruno Serrão <brunosserrao@gmail.com>
2021-01-30 14:45:31 -0300

    TabView as slide images

--- App Watch WatchKit Extension/ContentView.swift
+++ App Watch WatchKit Extension/ContentView.swift
@@ -6,11 +6,54 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+//    var body: some View {
+//        Text("Hello, World!")
+//            .padding();
+//    }
+    
+    @State private var selectedTab = 0
+    @State private var galadriel: Image = Image("galadriel");
+    @State private var gabriela: Image = Image("gabriela");
+    @State private var jujuba: Image = Image("jujuba");
+    
+    
+
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        TabView {
+            galadriel
+            .resizable()
+            .frame(width: 160.0, height: 160.0)
+            .onTapGesture {
+                print("Cliquei na Galadriel")
+                self.playCat(cat: "galadriel")
+            }
+            gabriela
+            .resizable()
+            .frame(width: 160.0, height: 160.0)
+            .onTapGesture {
+                print("Cliquei na Gabriela")
+                self.playCat(cat: "gabriela")
+            }
+            jujuba
+            .resizable()
+            .frame(width: 160.0, height: 160.0)
+            .onTapGesture {
+                print("Cliquei na Galadriel")
+                self.playCat(cat: "jujuba")
+            }
+        }
+    }
+    
+    func playCat(cat: String) {
+        if let meow = Bundle.main.path(forResource: cat, ofType: "mp3") {
+            _ = try? AVAudioPlayer(contentsOf: URL (fileURLWithPath: meow))
+        }
+        else {
+            print( cat + " audio não encontrado" )
+        }
     }
 }
 

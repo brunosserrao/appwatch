eb8d4c45837ad52899af3f6edb166bb4a608052a
Bruno Serrão <brunosserrao@gmail.com>
2021-02-02 16:12:25 -0300

    Cats came from an array of cats

--- App Watch WatchKit Extension/ContentView.swift
+++ App Watch WatchKit Extension/ContentView.swift
@@ -9,53 +9,33 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-//    var body: some View {
-//        Text("Hello, World!")
-//            .padding();
-//    }
-    
-    @State private var selectedTab = 0
-    @State private var galadriel: Image = Image("galadriel");
-    @State private var gabriela: Image = Image("gabriela");
-    @State private var jujuba: Image = Image("jujuba");
-    
     @State var aVAudioPlayer: AVAudioPlayer!
+    
+    let cats: [String] = ["galadriel", "gabriela", "jujuba"].shuffled()
 
     var body: some View {
+        
+        
         TabView {
-            galadriel
-            .resizable()
-            .frame(width: 160.0, height: 160.0)
-            .onTapGesture {
-                print("Cliquei na Galadriel")
-                playCat(cat: "galadriel")
-            }
-            gabriela
-            .resizable()
-            .frame(width: 160.0, height: 160.0)
-            .onTapGesture {
-                print("Cliquei na Gabriela")
-                playCat(cat: "gabriela")
-            }
-            jujuba
-            .resizable()
-            .frame(width: 160.0, height: 160.0)
-            .onTapGesture {
-                print("Cliquei na Jujuba")
-                playCat(cat: "jujuba")
+            ForEach(cats, id: \.self) { cat in
+                Image(cat)
+                .resizable()
+                .frame(width: 160.0, height: 160.0)
+                .onTapGesture {
+                    print("Cliquei na " + cat)
+                    playCat(cat: cat)
+                }
             }
         }
     }
     
     func playCat(cat: String) {
-        if let path = Bundle.main.path(forResource: cat, ofType: "mp3") {
-            do {
-                aVAudioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
-                aVAudioPlayer.play()
-            } catch {
-                print( "Could not find file")
-            }
-        }
+        let utterance = AVSpeechUtterance(string: cat)
+        utterance.voice = AVSpeechSynthesisVoice(language: "pt-BR")
+        utterance.rate = 0.1
+
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
     }
 }
 
